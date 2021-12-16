32c9a6f0515a86a22bb2dfa0e1dafd6e46248fb1
Antonio Thomacelli <tonnytg@gmail.com>
2021-12-16 12:43:18 -0300

    feat: segregate method save

--- main.go
+++ main.go
@@ -26,49 +26,38 @@ type Slot struct {
 	NextSlot *Slot
 }
 
-func main() {
+var m = make([]Cluster, 7)
 
+func init() {
 	// Cria o slice de cluster
-	m := make([]Cluster, 7)
+
 
 	// Alimenta o Cluster com os indices dos Nodes
 	for i := 0; i < 7; i++ {
 		m[i].ID = i
 		m[i].Node = &Node{ID: i}
 	}
+}
 
-	// Percorre o Cluster e imprime o Slot somente se tiver conteúdo
-	for i := 0; i < 7; i++ {
-		fmt.Println(m[i])
-		if m[i].Node.NodeSlot != nil {
-			fmt.Println(m[i].Node.NodeSlot)
-		}
-	}
-
-	nome := "Antonio Thomacelli Gomes"
-	fmt.Println("Será armazenado em:", Calc(nome))
-
-	// Cria o primeiro Slot e se estiver ocupado joga para o próximo
-    if m[Calc(nome)].Node.NodeSlot == nil {
-		m[Calc(nome)].Node.NodeSlot = &Slot{ID: Calc(nome), Value: nome}
+func SaveData(v string) {
+	if m[Calc(v)].Node.NodeSlot == nil {
+		m[Calc(v)].Node.NodeSlot = &Slot{ID: Calc(v), Value: v}
 	} else {
-		m[Calc(nome)].Node.NodeSlot.NextSlot = &Slot{ID: Calc(nome), Value: nome}
+		m[Calc(v)].Node.NodeSlot.NextSlot = &Slot{ID: Calc(v), Value: v}
 	}
+}
 
-	fmt.Println(m[Calc(nome)].Node.NodeSlot)
+func main() {
 
-	//fmt.Println(m)
-	//c := Calc(m[1])
-	//fmt.Println(c)
+	names := []string{"Antonio Thomacelli Gomes", "Antonio Thomacelli Gomez"}
 
-	//println("Start Hash Table")
-	//h := hashtable.NewHashTable(7)
-	//fmt.Println(h)
-	//h.Add("key1", "value1")
-	//h.Add("key2", "value2")
-	//h.Add("key3", "value3")
-	//
-	//h.Get("Tonny")
-	//
-	//h.Remove("key2")
+	for _, v := range names {
+		// Cria o primeiro Slot e se estiver ocupado joga para o próximo
+		SaveData(v)
+
+		fmt.Printf("Valor: %s\nSalvo no Cluster: %v\t\t " +
+			"Node:%v\t\t Slot: %v\n", v, Calc(v), m[Calc(v)].Node.ID, m[Calc(v)].Node.NodeSlot.ID)
+		fmt.Println(m[Calc(v)].Node.NodeSlot)
+		fmt.Println("---")
+	}
 }
